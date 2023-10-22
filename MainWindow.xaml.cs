aa60ba1f0e78ae0b9f95c0ca16f1a2e8345d2031
Ca349 <82281514+Ca349@users.noreply.github.com>
2023-10-22 13:09:09 +0100

    v0.1
    the first   
    
    probably the last*

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+
+            bool? response = openFileDialog.ShowDialog();
+
+
+            //Determines if the open button clicked, then ask for a file.
+
+            if (response == true)
+            {
+                string filepath = openFileDialog.FileName;
+
+                string outputpath = "Output/output.html/";
+
+                // Convert The File (I know one line of code lol)
+
+
+                File.Copy(filepath, outputpath, true);
+            }
+
+
+
+
+            {
+
+            }
+        }
+    }
+}
